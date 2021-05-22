ca1cedf156eefd3ba269ee3f68bade30d90d7aca
OderoOluoch <oderoluoch@gmail.com>
2021-05-22 13:32:51 +0300

    Set state wired to the text infput form component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import React, { useState } from "react"
 import './App.css';
 
 import Form from './components/Form';
 import ToDoList  from './components/ToDoList'
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
       <header>
         <h1>Odero : To Do List</h1>
       </header>
-      <Form />
+      <Form setInputText={setInputText}/>
       <ToDoList />
     </div>
   );
